69d4e7f0dc6e911700c20e809aaf39b2e25e151e
tonybarrel <antonio.barile2@studio.unibo.it>
2023-03-22 17:59:32 +0100

    closing the if commands

--- percolating/initial_mask.m
+++ percolating/initial_mask.m
@@ -5,17 +5,17 @@ function [mask]=initial_mask(N,M,p,s)
 %            s: type of generation seed
 %return the mask with extra borders
 if 0<=p && p<=1
+else
+    error 'valueError: Probability must be in the [0:1] interval'
+end   
     if N>1 && M>1
+    else 
+        error 'valueError: Both dimensions of the lattice must be > 1'
+    end    
         rng(s); 
         mask=rand(N,M)<p;
               % adding extra borders for boundaries problems
         mask = [zeros(1,size(mask,2)); mask; zeros(1,size(mask,2))]; 
         mask = [zeros(size(mask,1),1), mask, zeros(size(mask,1),1)];
-    else 
-        disp(['valueError: Both dimensions of the lattice must be > 1, but are ' num2str(N) ' and ' num2str(M)])
-    end
-else
-    disp('valueError: Probability must be in the [0:1] interval')
-end
 end
         
