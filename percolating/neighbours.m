62d628505cf2f2463aaf6216313f5fbbc06b2410
tonybarrel <antonio.barile2@studio.unibo.it>
2023-03-24 14:56:43 +0100

    modification of the regroup function and the neighbours one

--- percolating/neighbours.m
+++ percolating/neighbours.m
@@ -1,6 +1,6 @@
 function [min_neighbour,label] = neighbours(label,i,j)
 %this function analyze the 4 neighbours of a single element (i,j) 
-%find the minimum value among them excluding the 0 find if it is smaller than (i,j)
+%find the minimum value among them excluding the 0 
 %and replace all the non-zero neighbour with it.
 
 %Parameters = label: working matrix; i: row index; j: column index
@@ -16,7 +16,8 @@ function [min_neighbour,label] = neighbours(label,i,j)
         label(i+1, j) = A(1,2);
         label(i, j-1) = A(1,3);
         label(i, j+1) = A(1,4);
-
+        
+        
 
 end        
       
--- percolating/regroup.m
+++ percolating/regroup.m
@@ -1,36 +1,17 @@
 function [label,mask]=regroup(mask)
-%this function evaluates the sites of the initial mask and build a second
-%matrix (label) that consider the 4 nearest neighbours of the initial site,
-%assigning a counter value for non zero sites and connect them in a cluster
+%This function evaluates the sites of the initial mask and label built from
+%counter process function and applyes the smaller_neighbours and the
+%bigger_neighbours functions.
 %Parameters = mask: configuration created by initial_mask;
-%return the label matrix (and therefore the percolation pattern) and the initial
+%return the label matrix (therefore the percolation pattern) and the initial
 %mask
 
- counter=1;
- label=zeros(size(mask));
- for i=2:size(mask,1)-1
-    for j=2:size(mask,2)-1
-         if  mask(i,j)~=0
-             label(i,j)=counter;
-             counter=counter+1; 
-         end
-    end
- end
+[label,mask]=counter_process(mask);
 for i=2:size(label,1)-1
     for j=2:size(label,2)-1
         if  label(i,j)~=0
-            minimum=label(i,j); %verifying in the 4 nearest neighbours the minimum value of the label
-            [min_neighbour,label]=neighbours(label,i,j);
-            if min_neighbour <= minimum
-              minimum=min_neighbour;
-              copy=label(i,j);
-              label(i,j)=minimum;
-              [label]=substitute(label,copy,minimum);%use a substitute function in order to double check possible neighbours connected but not yet visualized
-            else
-               copy=min_neighbour;
-               
-               [label]=substitute(label,copy,minimum);
-            end
+            [label] = smaller_neighbours(label,i,j);
+            [label] = bigger_neighbours(label,i,j); 
         end
     end
 end
